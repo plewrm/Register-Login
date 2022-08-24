8cbeacc52621263f2214aea6879f743208367574
Pawan <salvepawan3@gmail.com>
2022-08-24 18:09:55 +0530

    Change css

---
+++ src/App.js
@@ -0,0 +1,40 @@
+// import logo from './logo.svg';
+// import React, { Fragment } from 'react'
+import './App.css';
+// import 'bootstrap/dist/css/bootstrap.min.css';
+// import Header from './Header';
+import Login from './Login'
+import Register from './Register'
+import AddProduct from './AddProduct'
+import UpdateProduct from './UpdateProduct'
+import { BrowserRouter, Routes, Route,Switch } from 'react-router-dom'
+import Protected from './Protected';
+import ProductList from './ProductList';
+import Search from './Search'
+
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+       
+          <Routes>
+            <Route path="/Login" element={<Login />}></Route>
+            <Route path="/register" element={<Register />}></Route>
+            
+            <Route path="/add" element={<Protected  Cmp={AddProduct}/>}/>
+            <Route path="/update/:id" element={<Protected  Cmp={UpdateProduct}/>}/>
+
+            <Route path="/search" element={<Protected  Cmp={Search}/>}/>
+            <Route path="/" element={<ProductList/>}></Route>
+            
+            </Routes>
+            
+          
+           
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
